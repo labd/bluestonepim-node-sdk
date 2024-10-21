4f515b98b6cd0a19eccf9c95450b35849d84516d
Thomas De Meyer <t.demeyer@labdigital.nl>
2024-10-21 09:01:00 +0200

    feat: initial commit

---
+++ src/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./AttributesApi";
+export * from "./CategoriesApi";
+export * from "./ContextsApi";
+export * from "./DifferencesApi";
+export * from "./HealthApi";
+export * from "./ProductsApi";
+export * from "./RelationsApi";
+export * from "./SyncsApi";
